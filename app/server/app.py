4bcd60d3104eb8cb28cd82a839dba32e01eff763
Aletsfly <alex17fly@gmail.com>
2025-08-07 18:05:21 -0600

    rutas crud y create

--- app/server/app.py
+++ app/server/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from server.routes.socie import router as SocieRouter
+
 app = FastAPI()
+
+app.include_router(SocieRouter, tags=["Socie"], prefix="/socie")
 
 @app.get("/", tags=['root'])
 async def read_root():
-    return {"message": "Bienvenido a la API de Socies"}
+    return {"message": "Bienvenides a esta fantástica aplicación"}
--- app/server/models/socie.py
+++ app/server/models/socie.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, Field, constr, conint, create_model
+from pydantic import BaseModel, EmailStr, Field, constr, conint
 
 
 class SchemaDeSocie(BaseModel):
@@ -9,10 +9,10 @@ class SchemaDeSocie(BaseModel):
     dni: conint(strict=True) = Field(...)
     nro_socie: conint(strict=True, gt=0) = Field(...)
     email: EmailStr = Field(...)
-    telefono: constr(strict=True) = Field(None)
-    direccion: constr(strict=True) = Field(None)
-    codigo_postal: constr(strict=True) = Field(None)
-    tipo_socio: bool = Field(...)
+    telefono: constr(strict=True) = Field()
+    direccion: constr(strict=True) = Field()
+    codigo_postal: constr(strict=True) = Field()
+    tipo_socio: bool = Field()
 
     class config:
         schema_extra = {
@@ -29,15 +29,32 @@ class SchemaDeSocie(BaseModel):
             }
         }
 
-    @classmethod
-    def as_optional(cls):
-        annonations = cls.__fields__
-        fields = {
-            attribute: (Optional[data_type.type_], None)
-            for attribute, data_type in annonations.items()
+
+class UpdateSocieModel(BaseModel):
+    nombre: Optional[constr(strict=True)]
+    apellido: Optional[constr(strict=True)]
+    dni: Optional[conint(strict=True)]
+    nro_socie: Optional[conint(strict=True, gt=0)]
+    email: Optional[EmailStr]
+    telefono: Optional[constr(strict=True)]
+    direccion: Optional[constr(strict=True)]
+    codigo_postal: Optional[constr(strict=True)]
+    tipo_socio: Optional[bool]
+
+    class Config:
+        schema_extra = {
+            "ejemplo": {
+                "nombre": "Juana",
+                "apellido": "Pilo",
+                "dni": 27358783,
+                "nro_socie": 1234,
+                "email": "jpilo@x.ar",
+                "telefono": "+54 9 456789",
+                "direccion": "calle pública S/n",
+                "codigo_postal": "5823",
+                "tipo_socio": True,
+            }
         }
-        OptionalModel = create_model(f"Optional{cls.__name__}", **fields)
-        return OptionalModel
 
 
 def ResponseModel(data, message):
